501c1c7787cb8ec164788d09f6a347afff8339ed
celinaidir <45858161+celinaidir@users.noreply.github.com>
2021-11-12 18:19:47 +0100

    Create Bike.java

---
+++ Bike.java
@@ -0,0 +1,14 @@
+
+public class Bike extends Vehicule{
+
+	
+	public Bike(String b) {
+		super(b,100);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
